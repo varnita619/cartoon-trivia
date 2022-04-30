14c702650a5b36fb34414f46a7e2641903f1028b
Varnita Makrariya <crabthe64@gmail.com>
2022-05-01 01:23:22 +0530

    refactor: changed css

--- src/Pages/Auth/Login.tsx
+++ src/Pages/Auth/Login.tsx
@@ -1,35 +1,42 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./Auth.css";
 
-export const Login = () =>{
-    return(
-        <div>
-             <main className="main-container">
+export const Login = () => {
+  return (
+    <div>
+      <main className="main-container">
         <div className="auth-container">
           <h1>Login</h1>
           <hr />
-  
-          <label><b>Email address</b></label>
+
+          <label>
+            <b>Email address</b>
+          </label>
           <input type="text" placeholder="Enter Email" name="email" required />
-  
-          <label><b>Password</b></label>
+
+          <label>
+            <b>Password</b>
+          </label>
           <input
             type="password"
             placeholder="*******"
             name="password"
             required
           />
-  
+
           <div className="clearfix">
-            <button type="submit" className="btn log-in-btn"><Link className="signup-btn" to="/">
-              Login</Link></button>
+            <button type="submit" className="btn log-in-btn">
+              <Link className="signup-btn" to="/">
+                Login
+              </Link>
+            </button>
           </div>
-  
+
           <div className="have-account">
             <Link to="/signup">Don't have an account </Link>
           </div>
         </div>
-    </main>
-        </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
